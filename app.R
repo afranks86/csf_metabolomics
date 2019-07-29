864856d5f91e5d69241277ffbf6c03a68a3d7627
nathanhwangbo <nathanhwangbo@umail.ucsb.edu>
2019-07-29 01:18:08 -0700

    adding plot folder, working on shiny app

--- app.R
+++ app.R
@@ -8,6 +8,10 @@ ui <- fluidPage(
     sidebarPanel(
       selectInput(inputId = 'response', label = 'Response', choices = c('AD', 'PD', 'Age')),
       selectInput(inputId = 'data', label = 'Dataset', choices = c('GOT', 'Lipids', 'Both'))
+    ),
+    mainPanel(
+      imageOutput(outputId = 'plot'),
+      textOutput(outputId = 'filename')
     )
   )
 )
@@ -28,9 +32,18 @@ ui <- fluidPage(
 
 
 server <- function(input, output){
+  plot_path <- 'plots'
+  output$filename <- reactive({
+    name <- character(0)
+    if(input$response == 'Age'){
+      name <- 'age'
+    }
+    paste(input$response, input$data, sep = '_')
+  })
   
-  
-  
+  output$plot <- renderImage({
+    list(src = 'plots/age_pred_resid_all_insample_4.png')
+  })
   
   
   
